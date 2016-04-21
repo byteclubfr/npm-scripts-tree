9cf10d1b822d89c5f5791bdff50a8996b12af76a
Delapouite <delapouite@gmail.com>
2016-04-21 12:30:53 +0200

    test: add assertions for getRunScripts and getRunAllScripts

---
+++ test/test.js
@@ -0,0 +1,46 @@
+var tap = require("tap");
+var nst = require("../index");
+
+// getRunScripts
+var runScripts = {
+  'foo': [],
+  'foo && npm run': [],
+  'foo && npm run bar': ['bar'],
+  'foo && npm run bar_toto': ['bar_toto'],
+  'foo && npm run bar-toto': ['bar-toto'],
+  'foo && npm run bar && qux': ['bar'],
+  'foo && npm run   bar': ['bar'],
+  'foo && npm run bar && npm run bar': ['bar'],
+  'foo && npm run bar && npm run qux': ['bar', 'qux'],
+  'foo && npm run bar && npm run   qux': ['bar', 'qux'],
+};
+
+Object.keys(runScripts).forEach((cmd) => {
+  tap.same(nst.getRunScripts(cmd), runScripts[cmd])
+});
+
+// getRunScripts
+var runAllScripts = {
+  'foo': [],
+  'foo && npm-run-all': [],
+  'foo && npm-run-all bar': ['bar'],
+  'foo && npm-run-all bar_toto': ['bar_toto'],
+  'foo && npm-run-all bar-toto': ['bar-toto'],
+  'foo && npm-run-all bar:toto': ['bar:toto'],
+  'foo && npm-run-all bar:*': ['bar:*'],
+  'foo && npm-run-all   bar': ['bar'],
+  'foo && npm-run-all bar bar': ['bar'],
+  'foo && npm-run-all bar qux': ['bar', 'qux'],
+  'foo && npm-run-all bar   qux': ['bar', 'qux'],
+  'foo && npm-run-all bar qux && npm-run-all yolo toto': ['bar', 'qux', 'yolo', 'toto'],
+  'foo && npm-run-all bar qux &&   npm-run-all yolo toto': ['bar', 'qux', 'yolo', 'toto'],
+  'foo && npm-run-all -s bar qux': ['bar', 'qux'],
+  'foo && npm-run-all --serial bar qux': ['bar', 'qux'],
+  'foo && npm-run-all -s bar qux && npm-run-all yolo': ['bar', 'qux', 'yolo'],
+  'foo && npm-run-all -p bar qux': ['bar', 'qux'],
+  'foo && npm-run-all --parallel bar qux': ['bar', 'qux'],
+};
+
+Object.keys(runAllScripts).forEach((cmd) => {
+  tap.same(nst.getRunAllScripts(cmd), runAllScripts[cmd])
+});
